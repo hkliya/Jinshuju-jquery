821d911bb91c3e41e2395a0636433711ac9cf9dc
Seaborn Lee <seabornlee.cn@gmail.com>
2016-06-07 11:03:41 +0800

    Delete element

--- js/handler.js
+++ js/handler.js
@@ -11,13 +11,19 @@ function confirmAddElement(elementType) {
     render();
 }
 
+function removeElement(index) {
+    console.log(index)
+    elements.splice(index, 1);
+    render();
+}
+
 function render() {
     var $form = $(".form");
     $form.empty();
     $.each(elements, function (_index, ele) {
         console.log(ele)
         $form.append($("<input type='" + ele + "' />"));
-        $form.append($("<button class='btn btn-danger btn-sm'>-</button>"));
+        $form.append($("<button name='btnDelete' class='btn btn-danger btn-sm' onclick='removeElement(" + _index + ")'>-</button>"));
         $form.append($("<br/>"));
     })
 }
